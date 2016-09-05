13fe3252c63b1a0c552b0d70ce6b8dc29388f6cf
ktsn <ktsn55@gmail.com>
2016-09-06 01:16:34 +0900

    Add basic examples

---
+++ examples/basic/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <input type="checkbox" v-model="show">Show Modal
+    <modal class="basic-modal" :show="show">
+      <h1 class="title">Modal</h1>
+      <button class="button" type="button" @click="show = false">Close</button>
+    </modal>
+  </div>
+</template>
+
+<script>
+import { Modal } from 'vuex-modal'
+
+export default {
+  data () {
+    return {
+      show: false
+    }
+  },
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style>
+.basic-modal {
+  margin: 30px auto;
+  padding: 30px;
+  width: 800px;
+}
+
+.title {
+  margin: 0;
+  font-weight: normal;
+}
+</style>
---
+++ examples/basic/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App'
+
+import 'vuex-modal/dist/vuex-modal.css'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
