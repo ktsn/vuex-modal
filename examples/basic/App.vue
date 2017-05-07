61df4da73428d1f65921ac59eaefb8ce14aeded6
ktsn <ktsn55@gmail.com>
2017-05-07 14:47:40 +0900

    Add long content example

--- examples/basic/App.vue
+++ examples/basic/App.vue
@@ -2,6 +2,7 @@
   <div>
     <input type="radio" name="modal" value="foo" v-model="current">Show Foo
     <input type="radio" name="modal" value="bar" v-model="current">Show Bar
+    <input type="radio" name="modal" value="long-content" v-model="current">Show Long Content
     <modal name="foo">
       <div class="basic-modal">
         <h1 class="title">Foo</h1>
@@ -12,6 +13,13 @@
       <div class="basic-modal">
         <h1 class="title">Bar</h1>
         <button class="button" type="button" @click="close">Close</button>
+      </div>
+    </modal>
+    <modal name="long-content">
+      <div class="basic-modal">
+        <h1 class="title">Long Content</h1>
+        <p>Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text </p>
+        <button class="button" @click="close">Close</button>
       </div>
     </modal>
   </div>
